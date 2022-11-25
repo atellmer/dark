eb0aaa186c512b7c0789821c9bcad1f634ffd687
atellmer <atellmer@gmail.com>
2022-11-25 00:25:20 -0800

    update

--- examples/track-updates/webpack.config.js
+++ examples/track-updates/webpack.config.js
@@ -27,7 +27,7 @@ const config = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+      'process.env.NODE_ENV': JSON.stringify('production'),
     }),
   ],
 };
