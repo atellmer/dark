f6e40e6e85a28d9c8e3b59fe0430e66e3ea73d2e
atellmer <atellmer@gmail.com>
2022-11-27 03:38:36 -0700

    added unmount tests

---
+++ packages/core/src/unmount/unmount.spec.tsx
@@ -0,0 +1,57 @@
+/** @jsx h */
+import { createRoot } from '@dark-engine/platform-browser';
+import { h } from '../element';
+import { createComponent } from '../component';
+import { useLayoutEffect } from '../use-layout-effect';
+import { useEffect } from '../use-effect';
+
+let host: HTMLElement = null;
+
+jest.useFakeTimers();
+
+beforeEach(() => {
+  host = document.createElement('div');
+});
+
+describe('[unmount]', () => {
+  test('unmount works correctly', () => {
+    const dropFn = jest.fn();
+
+    const Child = createComponent(() => {
+      useLayoutEffect(() => {
+        return () => dropFn();
+      }, []);
+
+      useEffect(() => {
+        return () => dropFn();
+      }, []);
+
+      return null;
+    });
+
+    const App = createComponent(() => {
+      useLayoutEffect(() => {
+        return () => dropFn();
+      }, []);
+
+      useEffect(() => {
+        return () => dropFn();
+      }, []);
+
+      return (
+        <div>
+          <Child />
+          <Child />
+          <Child />
+        </div>
+      );
+    });
+
+    const root = createRoot(host);
+
+    root.render(App());
+    jest.runAllTimers();
+    root.unmount();
+    expect(dropFn).toBeCalledTimes(8);
+  });
+});
