5cbd4cb6c9ac247a99946c2cde386529822ed028
atellmer <atellmer@gmail.com>
2022-11-27 01:50:33 -0800

    refactoring of use-state tests

--- packages/core/src/use-state/use-state.spec.tsx
+++ packages/core/src/use-state/use-state.spec.tsx
@@ -1,81 +1,74 @@
-/** @jsx createElement */
-import { dom, waitNextIdle, createEmptyCommentString } from '@test-utils';
+/** @jsx h */
+import { dom } from '@test-utils';
 import { render } from '@dark-engine/platform-browser/render';
+import { h } from '../element';
 import { createComponent } from '../component/component';
-import { createElement } from '../element/element';
 import { useState } from './use-state';
 
 let host: HTMLElement = null;
-const emptyComment = createEmptyCommentString();
-
-beforeAll(() => {
-  jest.useFakeTimers();
-});
 
 beforeEach(() => {
   host = document.createElement('div');
   jest.spyOn(window, 'requestAnimationFrame').mockImplementation((cb: Function) => cb());
 });
 
-test('[use-state]: use-state works correctly', () => {
-  const content = (count: number) => dom`
-    <div>${count}</div>
-  `;
-  let count;
-  let setCount;
-  const Component = createComponent(() => {
-    [count, setCount] = useState(0);
+describe('[use-state]', () => {
+  test('[use-state]: use-state works correctly', () => {
+    const content = (count: number) => dom`
+      <div>${count}</div>
+    `;
+    let count;
+    let setCount;
+    const Component = createComponent(() => {
+      [count, setCount] = useState(0);
 
-    return <div>{count}</div>;
+      return <div>{count}</div>;
+    });
+
+    render(Component(), host);
+    expect(host.innerHTML).toBe(content(0));
+
+    setCount(count + 1);
+    expect(host.innerHTML).toBe(content(1));
+
+    setCount(x => x + 1);
+    expect(host.innerHTML).toBe(content(2));
+
+    setCount(x => x + 1);
+    setCount(x => x + 1);
+    expect(host.innerHTML).toBe(content(4));
   });
 
-  render(Component(), host);
-  waitNextIdle();
-  expect(host.innerHTML).toBe(content(0));
-  setCount(count + 1);
-  waitNextIdle();
-  expect(host.innerHTML).toBe(content(1));
-  setCount(x => x + 1);
-  waitNextIdle();
-  expect(host.innerHTML).toBe(content(2));
-  setCount(x => x + 1);
-  waitNextIdle();
-  setCount(x => x + 1);
-  waitNextIdle();
-  expect(host.innerHTML).toBe(content(4));
-});
+  test('[use-state]: use-state works correctly when component returns array of elements', () => {
+    const content = (count: number) => dom`
+      <div>text</div>
+      <div>${count}</div>
+    `;
+    let count: number;
+    let setCount: (value: number) => void;
+    const Component = createComponent(() => {
+      [count, setCount] = useState(0);
 
-test('[use-state]: use-state works correctly when component returns array of elements', () => {
-  const content = (count: number) => dom`
-    <div>text</div>
-    <div>${count}</div>
-  `;
-  let count;
-  let setCount;
-  const Component = createComponent(() => {
-    [count, setCount] = useState(0);
+      return [<div>text</div>, <div>{count}</div>];
+    });
 
-    return [<div>text</div>, <div>{count}</div>];
+    render(Component(), host);
+    expect(host.innerHTML).toBe(content(0));
+
+    setCount(count + 1);
+    expect(host.innerHTML).toBe(content(1));
+
+    setCount(count + 1);
+    expect(host.innerHTML).toBe(content(2));
   });
 
-  render(Component(), host);
-  waitNextIdle();
-  expect(host.innerHTML).toBe(content(0));
-  setCount(count + 1);
-  waitNextIdle();
-  expect(host.innerHTML).toBe(content(1));
-  setCount(count + 1);
-  waitNextIdle();
-  expect(host.innerHTML).toBe(content(2));
-});
+  test('[use-state]: state saves when nodes swapped', () => {
+    type Item = {
+      id: number;
+      count: number;
+    };
 
-test('[use-state]: state saves when nodes swapped', () => {
-  type Item = {
-    id: number;
-    count: number;
-  };
-
-  const content = (items: Array<Item>) => dom`
+    const content = (items: Array<Item>) => dom`
     ${items
       .map(x => {
         return `
@@ -88,110 +81,101 @@ test('[use-state]: state saves when nodes swapped', () => {
       .join('')}
   `;
 
-  const items = Array(5)
-    .fill(null)
-    .map((_, idx) => ({
-      id: idx + 1,
-      count: 0,
-    }));
+    const items = Array(5)
+      .fill(null)
+      .map((_, idx) => ({
+        id: idx + 1,
+        count: 0,
+      }));
 
-  let setCountsOne = [];
-  let setCountsTwo = [];
+    let setCountsOne = [];
+    let setCountsTwo = [];
 
-  const CounterOne = createComponent<{ id: number }>(({ id }) => {
-    const [count, setCount] = useState(0);
+    const CounterOne = createComponent<{ id: number }>(({ id }) => {
+      const [count, setCount] = useState(0);
 
-    setCountsOne.push(setCount);
+      setCountsOne.push(setCount);
 
-    return (
-      <div>
+      return (
         <div>
-          id: {id}, count: {count}
+          <div>
+            id: {id}, count: {count}
+          </div>
+          <CounterTwo />
         </div>
-        <CounterTwo />
-      </div>
-    );
-  });
-
-  const CounterTwo = createComponent(() => {
-    const [count, setCount] = useState(0);
-
-    setCountsTwo.push(setCount);
-
-    return <div>{count}</div>;
-  });
-
-  const List = createComponent(() => {
-    return items.map(x => {
-      return <CounterOne key={x.id} id={x.id} />;
+      );
     });
+
+    const CounterTwo = createComponent(() => {
+      const [count, setCount] = useState(0);
+
+      setCountsTwo.push(setCount);
+
+      return <div>{count}</div>;
+    });
+
+    const List = createComponent(() => {
+      return items.map(x => {
+        return <CounterOne key={x.id} id={x.id} />;
+      });
+    });
+
+    const swap = () => {
+      const temp = items[1];
+      items[1] = items[items.length - 2];
+      items[items.length - 2] = temp;
+    };
+
+    render(List(), host);
+    expect(host.innerHTML).toBe(content(items));
+
+    setCountsOne[1](1);
+    setCountsTwo[1](1);
+    items[1].count = 1;
+    setCountsOne[items.length - 2](2);
+    setCountsTwo[items.length - 2](2);
+    items[items.length - 2].count = 2;
+    expect(host.innerHTML).toBe(content(items));
+
+    setCountsOne = [];
+    setCountsTwo = [];
+    swap();
+    render(List(), host);
+    expect(host.innerHTML).toBe(content(items));
+
+    setCountsOne = [];
+    setCountsTwo = [];
+    swap();
+    render(List(), host);
+    expect(host.innerHTML).toBe(content(items));
   });
 
-  const swap = () => {
-    const temp = items[1];
-    items[1] = items[items.length - 2];
-    items[items.length - 2] = temp;
-  };
+  test('[use-state]: nodes remove correctly after use-state when rendering a different number of elements', () => {
+    const content = (hasFlag: boolean) =>
+      hasFlag
+        ? dom`<div>flag</div>`
+        : dom`
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+          `;
+    let hasFlag: boolean;
+    let setHasFlag: (value: boolean) => void;
+    const Component = createComponent(() => {
+      [hasFlag, setHasFlag] = useState(false);
 
-  render(List(), host);
-  waitNextIdle();
-  expect(host.innerHTML).toBe(content(items));
+      if (hasFlag) return <div>flag</div>;
 
-  setCountsOne[1](1);
-  waitNextIdle();
-  setCountsTwo[1](1);
-  waitNextIdle();
-  items[1].count = 1;
-  setCountsOne[items.length - 2](2);
-  waitNextIdle();
-  setCountsTwo[items.length - 2](2);
-  items[items.length - 2].count = 2;
-  waitNextIdle();
-  expect(host.innerHTML).toBe(content(items));
-  setCountsOne = [];
-  setCountsTwo = [];
-  swap();
+      return [<div>1</div>, <div>2</div>, <div>3</div>];
+    });
 
-  render(List(), host);
-  waitNextIdle();
-  expect(host.innerHTML).toBe(content(items));
-  setCountsOne = [];
-  setCountsTwo = [];
-  swap();
+    render(Component(), host);
+    expect(host.innerHTML).toBe(content(false));
 
-  render(List(), host);
-  waitNextIdle();
-  expect(host.innerHTML).toBe(content(items));
-  setCountsOne = [];
-  setCountsTwo = [];
-});
+    setHasFlag(true);
+    expect(host.innerHTML).toBe(content(true));
 
-test('[use-state]: nodes remove correctly after use-state when rendering a different number of elements', () => {
-  const content = (hasFlag: boolean) =>
-    hasFlag
-      ? dom`<div>flag</div>`
-      : dom`
-    <div>1</div>
-    <div>2</div>
-    <div>3</div>
-  `;
-  let hasFlag;
-  let setHasFlag;
-  const Component = createComponent(() => {
-    [hasFlag, setHasFlag] = useState(false);
-
-    if (hasFlag) return <div>flag</div>;
-
-    return [<div>1</div>, <div>2</div>, <div>3</div>];
+    setHasFlag(false);
+    expect(host.innerHTML).toBe(content(false));
   });
-
-  render(Component(), host);
-  waitNextIdle();
-  expect(host.innerHTML).toBe(content(false));
-  setHasFlag(true);
-  waitNextIdle();
-  expect(host.innerHTML).toBe(content(true));
-  setHasFlag(false);
-  waitNextIdle();
-  expect(host.innerHTML).toBe(content(false));
 });
