6cde1fafd2a20aa4f72a969f18f7246d83eca2ee
atellmer <atellmer@gmail.com>
2022-11-27 05:56:51 -0700

    added use-ref tests

---
+++ packages/core/src/use-ref/use-ref.spec.tsx
@@ -0,0 +1,28 @@
+/** @jsx h */
+import { render } from '@dark-engine/platform-browser';
+import { h } from '../element';
+import { createComponent } from '../component';
+import { MutableRef } from '../ref';
+import { useRef } from './use-ref';
+
+let host: HTMLElement = null;
+
+beforeEach(() => {
+  host = document.createElement('div');
+});
+
+describe('[useRef]', () => {
+  test('useRef works correctly', () => {
+    let ref: MutableRef<HTMLDivElement> = null;
+
+    const App = createComponent(() => {
+      ref = useRef(null);
+
+      return <div ref={ref} />;
+    });
+
+    render(App(), host);
+    expect(ref.current).toBeTruthy();
+    expect(ref.current instanceof HTMLDivElement).toBeTruthy();
+  });
+});
