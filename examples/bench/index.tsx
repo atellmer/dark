e2e936ab2fda99e10faf88cee72590fb2bea9a3b
atellmer <atellmer@gmail.com>
2022-11-30 05:20:43 -0700

    fix bench

--- examples/bench/index.tsx
+++ examples/bench/index.tsx
@@ -1,176 +1,259 @@
-import { h, Fragment, createComponent, useState } from '@dark-engine/core';
-import { createRoot, type SyntheticEvent } from '@dark-engine/platform-browser';
+import { h, View, Text, Fragment, createComponent, memo, useCallback } from '@dark-engine/core';
+import { createRoot } from '@dark-engine/platform-browser';
 
-const App = createComponent(() => {
-  const [textInputValue, setTextInputValue] = useState('hello');
-  const [passwordInputValue, setPasswordInputValue] = useState('12345');
-  const [numberInputValue, setNumberInputValue] = useState(10);
-  const [colorInputValue, setColorInputValue] = useState('#8260d2');
-  const [textareaValue, setTextareaValue] = useState('world');
-  const [checkboxInputValue, setCheckboxInputValue] = useState(true);
-  const [radioInputValue, setRadioInputValue] = useState('phone');
-  const [selectValue, setSelectValue] = useState('🐱');
-  const [multiSelectValue, setMultiSelectValue] = useState(['🍎', '🍉']);
+const div = (props = {}) => View({ ...props, as: 'div' });
+const button = (props = {}) => View({ ...props, as: 'button' });
+
+const createMeasurer = () => {
+  let startTime;
+  let lastMeasureName: string;
+  const start = (name: string) => {
+    startTime = performance.now();
+    lastMeasureName = name;
+  };
+  const stop = () => {
+    const last = lastMeasureName;
+
+    if (lastMeasureName) {
+      setTimeout(() => {
+        lastMeasureName = null;
+        const stopTime = performance.now();
+        const diff = stopTime - startTime;
+
+        console.log(`${last}: ${diff}`);
+      });
+    }
+  };
+
+  return {
+    start,
+    stop,
+  };
+};
+
+const measurer = createMeasurer();
+
+let nextId = 0;
+const buildData = (count, prefix = '') => {
+  return Array(count)
+    .fill(0)
+    .map(() => ({
+      id: ++nextId,
+      name: `item: ${nextId} ${prefix}`,
+      select: false,
+    }));
+};
+
+const state = {
+  list: [],
+};
+
+type HeaderProps = {
+  onCreate: () => void;
+  onPrepend: () => void;
+  onAppend: () => void;
+  onInsertDifferent: () => void;
+  onUpdateAll: () => void;
+  onSwap: () => void;
+  onClear: () => void;
+};
+
+const Header = createComponent<HeaderProps>(
+  ({ onCreate, onPrepend, onAppend, onInsertDifferent, onUpdateAll, onSwap, onClear }) => {
+    return div({
+      style:
+        'width: 100%; height: 64px; background-color: blueviolet; display: flex; align-items: center; padding: 16px;',
+      slot: [
+        button({
+          slot: Text('create 10000 rows'),
+          onClick: onCreate,
+        }),
+        button({
+          slot: Text('Prepend 1000 rows'),
+          onClick: onPrepend,
+        }),
+        button({
+          slot: Text('Append 1000 rows'),
+          onClick: onAppend,
+        }),
+        button({
+          slot: Text('insert different'),
+          onClick: onInsertDifferent,
+        }),
+        button({
+          slot: Text('update every 10th row'),
+          onClick: onUpdateAll,
+        }),
+        button({
+          slot: Text('swap rows'),
+          onClick: onSwap,
+        }),
+        button({
+          slot: Text('clear rows'),
+          onClick: onClear,
+        }),
+        button({
+          slot: Text('unmount app'),
+          onClick: () => {
+            root.unmount();
+          },
+        }),
+      ],
+    });
+  },
+);
+
+const MemoHeader = memo<HeaderProps>(Header);
+
+type RowProps = {
+  id: number;
+  name: string;
+  selected: boolean;
+  onRemove: (id: number) => void;
+  onHighlight: (id: number) => void;
+};
+
+const Row = createComponent<RowProps>(({ id, name, selected, onRemove, onHighlight }) => {
+  const handleRemove = useCallback(() => onRemove(id), []);
+  const handleHighlight = useCallback(() => onHighlight(id), []);
+
+  return (
+    <tr class={selected ? 'selected' : undefined}>
+      <td class='cell'>{name}</td>
+      <td class='cell'>qqq</td>
+      <td class='cell'>xxx</td>
+      <td class='cell'>
+        <button onClick={handleRemove}>remove</button>
+        <button onClick={handleHighlight}>highlight</button>
+      </td>
+    </tr>
+  );
+});
+
+const MemoRow = memo<RowProps>(
+  Row,
+  (props, nextProps) => props.name !== nextProps.name || props.selected !== nextProps.selected,
+);
+
+type ListProps = {
+  items: Array<{ id: number; name: string; select: boolean }>;
+  onRemove: (id: number) => void;
+  onHighlight: (id: number) => void;
+};
+
+const List = createComponent<ListProps>(({ items, onRemove, onHighlight }) => {
+  return (
+    <table class='table'>
+      <tbody>
+        {items.map(item => {
+          return (
+            <MemoRow
+              key={item.id}
+              id={item.id}
+              name={item.name}
+              selected={item.select}
+              onRemove={onRemove}
+              onHighlight={onHighlight}
+            />
+          );
+        })}
+      </tbody>
+    </table>
+  );
+});
+
+const MemoList = memo(List);
+
+const Bench = createComponent(() => {
+  const handleCreate = useCallback(() => {
+    state.list = buildData(10000);
+    measurer.start('create');
+    forceUpdate();
+    measurer.stop();
+  }, []);
+  const handlePrepend = useCallback(() => {
+    state.list.unshift(...buildData(1000, '!!!'));
+    state.list = [...state.list];
+    measurer.start('prepend');
+    forceUpdate();
+    measurer.stop();
+  }, []);
+  const handleAppend = useCallback(() => {
+    state.list.push(...buildData(1000, '!!!'));
+    state.list = [...state.list];
+    measurer.start('append');
+    forceUpdate();
+    measurer.stop();
+  }, []);
+  const handleInsertDifferent = useCallback(() => {
+    const [item1, item2, item3, ...rest] = state.list;
+
+    state.list = [...buildData(5, '***'), item1, item2, item3, ...buildData(2, '***'), ...rest].filter(Boolean);
+    measurer.start('insert different');
+    forceUpdate();
+    measurer.stop();
+  }, []);
+  const handleUpdateAll = useCallback(() => {
+    state.list = state.list.map((x, idx) => ({ ...x, name: (idx + 1) % 10 === 0 ? x.name + '!!!' : x.name }));
+    measurer.start('update every 10th');
+    forceUpdate();
+    measurer.stop();
+  }, []);
+  const handleRemove = useCallback(id => {
+    state.list = state.list.filter(x => x.id !== id);
+    measurer.start('remove');
+    forceUpdate();
+    measurer.stop();
+  }, []);
+  const handleHightlight = useCallback(id => {
+    const idx = state.list.findIndex(x => x.id === id);
+    state.list[idx].select = !state.list[idx].select;
+    state.list = [...state.list];
+    measurer.start('highlight');
+    forceUpdate();
+    measurer.stop();
+  }, []);
+  const handleSwap = useCallback(() => {
+    if (state.list.length === 0) return;
+    const temp = state.list[1];
+    state.list[1] = state.list[state.list.length - 2];
+    state.list[state.list.length - 2] = temp;
+    state.list = [...state.list];
+    measurer.start('swap');
+    forceUpdate();
+    measurer.stop();
+  }, []);
+  const handleClear = useCallback(() => {
+    state.list = [];
+    measurer.start('clear');
+    forceUpdate();
+    measurer.stop();
+  }, []);
 
   return (
     <>
-      <h1>HTML input elements</h1>
-      <hr />
-      <div>
-        html5 form validation
-        <form
-          onSubmit={(e: SyntheticEvent<InputEvent, HTMLFormElement>) => {
-            e.preventDefault();
-            alert(
-              JSON.stringify({
-                login: e.target['login'].value,
-                password: e.target['password'].value,
-              }),
-            );
-          }}>
-          <input
-            type='text'
-            name='login'
-            autoFocus
-            maxLength={10}
-            data-item={true}
-            placeholder='login (max length 10)'
-            required
-          />
-          <input type='password' name='password' placeholder='password' required />
-          <input type='submit' value='Sign in' />
-        </form>
-      </div>
-      <hr />
-      <div>
-        <div>disabled input</div>
-        <input value='hello' disabled />
-      </div>
-      <hr />
-      <div>
-        <div>readonly input</div>
-        <input value='hello' readOnly />
-      </div>
-      <hr />
-      <div>
-        <div>text input: {textInputValue}</div>
-        <input
-          type='text'
-          value={textInputValue}
-          required
-          onInput={(e: SyntheticEvent<InputEvent, HTMLInputElement>) => setTextInputValue(e.target.value)}
-        />
-      </div>
-      <hr />
-      <div>
-        <div>password input: {passwordInputValue}</div>
-        <input
-          type='password'
-          value={passwordInputValue}
-          onInput={(e: SyntheticEvent<InputEvent, HTMLInputElement>) => setPasswordInputValue(e.target.value)}
-        />
-      </div>
-      <hr />
-      <div>
-        <div>number input: {numberInputValue}</div>
-        <input
-          type='number'
-          value={numberInputValue}
-          onInput={(e: SyntheticEvent<InputEvent, HTMLInputElement>) => setNumberInputValue(Number(e.target.value))}
-        />
-      </div>
-      <hr />
-      <div>
-        <div>color input: {colorInputValue}</div>
-        <input
-          type='color'
-          value={colorInputValue}
-          onInput={(e: SyntheticEvent<InputEvent, HTMLInputElement>) => setColorInputValue(e.target.value)}
-        />
-      </div>
-      <hr />
-      <div>
-        <div>textarea: {textareaValue}</div>
-        <textarea
-          value={textareaValue}
-          onInput={(e: SyntheticEvent<InputEvent, HTMLTextAreaElement>) => setTextareaValue(e.target.value)}></textarea>
-      </div>
-      <hr />
-      <div>
-        <label>
-          <input
-            type='checkbox'
-            value={checkboxInputValue}
-            onChange={(e: SyntheticEvent<InputEvent, HTMLInputElement>) => setCheckboxInputValue(!checkboxInputValue)}
-          />
-          {checkboxInputValue ? 'checked' : 'not cheked'}
-        </label>
-      </div>
-      <hr />
-      <div>
-        <div>radio input: {radioInputValue}</div>
-        {['email', 'phone', 'mail'].map(x => {
-          return (
-            <label>
-              <input
-                type='radio'
-                name='contact'
-                value={x}
-                checked={radioInputValue === x}
-                onChange={(e: SyntheticEvent<InputEvent, HTMLInputElement>) => setRadioInputValue(e.target.value)}
-              />
-              {x}
-            </label>
-          );
-        })}
-      </div>
-      <hr />
-      <div>
-        <div>select: {selectValue}</div>
-        <label>
-          Choose a pet:
-          <br />
-          <select
-            name='pet'
-            style='width: 200px'
-            onChange={(e: SyntheticEvent<InputEvent, HTMLSelectElement>) => setSelectValue(e.target.value)}>
-            {['🐶', '🐱', '🐭'].map(x => {
-              return (
-                <option value={x} selected={selectValue === x}>
-                  {x}
-                </option>
-              );
-            })}
-          </select>
-        </label>
-      </div>
-      <hr />
-      <div>
-        <div>multiselect: {JSON.stringify(multiSelectValue)}</div>
-        <label>
-          Choose a fruit:
-          <br />
-          <select
-            name='fruit'
-            style='width: 200px'
-            multiple
-            onChange={(e: SyntheticEvent<InputEvent, HTMLSelectElement>) =>
-              setMultiSelectValue(Array.from(e.target.selectedOptions).map(x => x.value))
-            }>
-            {['🍎', ' 🍌', '🍉', '🍓'].map(x => {
-              const isSelected = multiSelectValue.includes(x);
-
-              return (
-                <option value={x} selected={isSelected}>
-                  {x}
-                </option>
-              );
-            })}
-          </select>
-        </label>
-      </div>
+      <MemoHeader
+        onCreate={handleCreate}
+        onPrepend={handlePrepend}
+        onAppend={handleAppend}
+        onInsertDifferent={handleInsertDifferent}
+        onUpdateAll={handleUpdateAll}
+        onSwap={handleSwap}
+        onClear={handleClear}
+      />
+      <MemoList items={state.list} onRemove={handleRemove} onHighlight={handleHightlight} />
     </>
   );
 });
 
-createRoot(document.getElementById('root')).render(<App />);
+const root = createRoot(document.getElementById('root'));
+
+function forceUpdate() {
+  root.render(<Bench />);
+}
+
+forceUpdate();
+
+document.querySelector('#button').addEventListener('click', () => {
+  forceUpdate();
+});
