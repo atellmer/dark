ab8957f652e8652b08b35e7e4896fa550ab24c32
atellmer <atellmer@gmail.com>
2022-11-30 09:26:06 -0700

    version upgrade

--- packages/core/types/use-sync-external-store/use-sync-external-store.d.ts
+++ packages/core/types/use-sync-external-store/use-sync-external-store.d.ts
@@ -1,4 +1,4 @@
-declare type Sunscribe = (cb: () => void) => Unsubscribe;
+declare type Subscribe = (cb: () => void) => Unsubscribe;
 declare type Unsubscribe = () => void;
-declare function useSyncExternalStore<T>(subscribe: Sunscribe, getSnapshot: () => T): T;
+declare function useSyncExternalStore<T>(subscribe: Subscribe, getSnapshot: () => T): T;
 export { useSyncExternalStore };
