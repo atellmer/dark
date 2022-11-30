37df44183420f35fbabf7f55d662f19b698e7ef8
atellmer <atellmer@gmail.com>
2022-11-30 05:41:09 -0700

    build

--- examples/todo-app/index.tsx
+++ examples/todo-app/index.tsx
@@ -1,5 +1,5 @@
-import { View, Text, createComponent, useState, useMemo, useRef, forwardRef } from '@dark-engine/core';
-import { render, type SyntheticEvent } from '@dark-engine/platform-browser';
+import { View, Text, createComponent, useState, useRef, forwardRef } from '@dark-engine/core';
+import { createRoot, type SyntheticEvent } from '@dark-engine/platform-browser';
 
 const div = (props = {}) => View({ ...props, as: 'div' });
 const button = (props = {}) => View({ ...props, as: 'button' });
@@ -29,9 +29,11 @@ type TextFieldProps = {
 const TextField = forwardRef(
   createComponent<TextFieldProps, HTMLInputElement>((props, ref) => {
     const { value, fulllWidth, onEnter, onChange } = props;
+
     const handleChange = (e: SyntheticEvent<InputEvent, HTMLInputElement>) => {
       onChange(e, e.target.value);
     };
+
     const handleKeyDown = (e: SyntheticEvent<KeyboardEvent, HTMLInputElement>) => {
       onEnter && e.sourceEvent.key === 'Enter' && onEnter(e);
     };
@@ -54,14 +56,15 @@ type CheckboxProps = {
 
 const Checkbox = createComponent<CheckboxProps>(props => {
   const { value, labelText, onChange } = props;
-  const handleInput = (e: SyntheticEvent<KeyboardEvent, HTMLInputElement>) => onChange(e, !value);
+
+  const handleInput = (e: SyntheticEvent<InputEvent, HTMLInputElement>) => onChange(e, !value);
 
   return label({
     slot: [
       input({
         style: 'margin-right: 10px',
         type: 'checkbox',
-        checked: value,
+        value: value,
         onInput: handleInput,
       }),
       Text(labelText),
@@ -77,7 +80,9 @@ type TaskItemProps = {
 
 const TaskItem = createComponent<TaskItemProps>(props => {
   const { task, onComplete, onRemove } = props;
+
   const handleCompleted = (_, completed: boolean) => onComplete(task.id, completed);
+
   const handleRemove = () => onRemove(task.id);
 
   return div({
@@ -87,16 +92,15 @@ const TaskItem = createComponent<TaskItemProps>(props => {
         style: 'display: flex;',
         slot: [
           div({
-            style: 'margin-right: 10px;',
+            style: `margin-right: 10px; ${task.completed ? 'text-decoration: line-through;' : ''}`,
             slot: [
               Checkbox({
                 value: task.completed,
-                labelText: task.completed ? 'yes' : 'no',
+                labelText: task.name,
                 onChange: handleCompleted,
               }),
             ],
           }),
-          Text(task.name),
         ],
       }),
       button({
@@ -107,30 +111,30 @@ const TaskItem = createComponent<TaskItemProps>(props => {
   });
 });
 
-const TodoApp = createComponent(() => {
-  const sourseTasks = useMemo(
-    () => [
-      new TodoTask('Learn Dark', true),
-      new TodoTask('Learn React', true),
-      new TodoTask('Learn Angular'),
-      new TodoTask('Learn Vue'),
-    ],
-    [],
-  );
-  const [tasks, setTasks] = useState<Array<TodoTask>>(sourseTasks);
+const App = createComponent(() => {
+  const [tasks, setTasks] = useState<Array<TodoTask>>(() => [
+    new TodoTask('Learn Dark', true),
+    new TodoTask('Learn React', true),
+    new TodoTask('Learn Angular'),
+    new TodoTask('Learn Vue'),
+  ]);
   const [taskName, setTaskName] = useState('');
   const textFieldRef = useRef<HTMLInputElement>(null);
+
   const handleChangeTaskName = (_, value: string) => setTaskName(value);
+
   const handleAddTask = () => {
     if (!taskName) return;
     setTasks([new TodoTask(taskName), ...tasks]);
     setTaskName('');
     textFieldRef.current.focus();
   };
+
   const handleComplete = (id: number, completed: boolean) => {
     tasks.find(x => x.id === id).completed = completed;
     setTasks([...tasks]);
   };
+
   const handleRemove = (id: number) => setTasks(tasks.filter(x => x.id !== id));
 
   return div({
@@ -166,4 +170,4 @@ const TodoApp = createComponent(() => {
   });
 });
 
-render(TodoApp(), document.getElementById('root'));
+createRoot(document.getElementById('root')).render(App());
