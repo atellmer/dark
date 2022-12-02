f6d46501ae57062f62af0e0a73ff82aa2cb1049d
atellmer <atellmer@gmail.com>
2022-12-02 00:00:07 -0700

    added events tests

--- packages/platform-browser/src/events/events.spec.tsx
+++ packages/platform-browser/src/events/events.spec.tsx
@@ -4,10 +4,16 @@ import { render } from '../render';
 
 let host: HTMLElement = null;
 
+const addEventListener = document.addEventListener.bind(document);
+
 beforeEach(() => {
   host?.parentElement === document.body && document.body.removeChild(host);
   host = document.createElement('div');
   document.body.appendChild(host);
+});
+
+afterEach(() => {
+  document.addEventListener = addEventListener;
 });
 
 const click = (element: Element) => {
@@ -22,6 +28,7 @@ const click = (element: Element) => {
 describe('[events]', () => {
   test('can fire events', () => {
     const mockFn = jest.fn();
+    let button: HTMLButtonElement = null;
 
     const render$ = () => {
       render(App(), host);
@@ -34,7 +41,7 @@ describe('[events]', () => {
     });
 
     render$();
-    const button = host.querySelector('button');
+    button = host.querySelector('button');
     click(button);
     expect(mockFn).toHaveBeenCalledTimes(1);
 
@@ -48,6 +55,7 @@ describe('[events]', () => {
 
   test('can remove event listeners when unmount', () => {
     const mockFn = jest.fn();
+    let button: HTMLButtonElement = null;
 
     const App = createComponent(() => {
       const handleClick = () => mockFn();
@@ -56,12 +64,34 @@ describe('[events]', () => {
     });
 
     render(App(), host);
-    const button = host.querySelector('button');
+    button = host.querySelector('button');
     click(button);
     expect(mockFn).toHaveBeenCalledTimes(1);
 
     render(null, host);
     click(button);
     expect(mockFn).toHaveBeenCalledTimes(1);
+  });
+
+  test('delegates all events to document', () => {
+    const mockFn1 = jest.fn();
+    const mockFn2 = jest.fn();
+
+    document.addEventListener = function (...args: Array<any>) {
+      mockFn1();
+      return addEventListener(...args);
+    };
+
+    const App = createComponent(() => {
+      const handleClick = () => mockFn2();
+
+      return <button onClick={handleClick}>click</button>;
+    });
+
+    render(App(), host);
+    click(host.querySelector('button'));
+    expect(mockFn1).toHaveBeenCalledTimes(1);
+    expect(mockFn2).toHaveBeenCalledTimes(1);
+    expect(mockFn1.mock.invocationCallOrder[0]).toBeLessThan(mockFn2.mock.invocationCallOrder[0]);
   });
 });
