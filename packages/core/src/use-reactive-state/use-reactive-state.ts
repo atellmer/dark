3f0db3d80ae38889b8f8106cb96a06ae31fa85f2
atellmer <atellmer@gmail.com>
2022-12-12 08:43:05 -0700

    x

--- packages/core/src/use-reactive-state/use-reactive-state.ts
+++ packages/core/src/use-reactive-state/use-reactive-state.ts
@@ -1,10 +1,7 @@
 import { detectIsObject, detectIsNull } from '../helpers';
 import { useUpdate } from '../use-update';
 import { useMemo } from '../use-memo';
-
-type Scope = {
-  timerId: number | null;
-};
+import { batch } from '../batch';
 
 function useReactiveState<T extends object>(value: T) {
   if (!value) {
@@ -12,13 +9,12 @@ function useReactiveState<T extends object>(value: T) {
   }
 
   const update = useUpdate();
-  const scope = useMemo<Scope>(() => ({ timerId: null }), []);
-  const reactiveValue = useMemo(() => reactive(value, scope, update), []);
+  const reactiveValue = useMemo(() => reactive(value, update), []);
 
   return reactiveValue;
 }
 
-function reactive<T extends object>(value: T, scope: Scope, update: () => void): T {
+function reactive<T extends object>(value: T, update: () => void): T {
   let patched: T = value;
 
   if (detectIsObject(value) && !detectIsNull(value)) {
@@ -26,10 +22,9 @@ function reactive<T extends object>(value: T, scope: Scope, update: () => void):
       set: function (target, prop, value) {
         if (target[prop] === value) return true;
 
-        target[prop] = reactive(value, scope, update);
+        target[prop] = reactive(value, update);
 
-        scope.timerId && window.clearTimeout(scope.timerId);
-        scope.timerId = window.setTimeout(() => {
+        batch(() => {
           update();
         });
 
@@ -38,7 +33,7 @@ function reactive<T extends object>(value: T, scope: Scope, update: () => void):
     });
 
     for (const key of Object.keys(value)) {
-      value[key] = reactive(value[key], scope, update);
+      value[key] = reactive(value[key], update);
     }
   }
 
