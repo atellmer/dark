60409db1173959248561fff7331f55ea719e22bd
atellmer <atellmer@gmail.com>
2022-12-13 02:53:48 -0700

    added factory tests

---
+++ packages/platform-browser/src/factory/factory.spec.tsx
@@ -0,0 +1,19 @@
+/** @jsx h */
+import { h, detectIsVirtualNodeFactory, TagVirtualNode } from '@dark-engine/core';
+import { factory } from './factory';
+
+describe('[factory]', () => {
+  test('can create factory', () => {
+    const div = factory('div');
+
+    expect(detectIsVirtualNodeFactory(div())).toBe(true);
+  });
+
+  test('returns tag correctly', () => {
+    const div = factory('div');
+    const compiled = div()();
+
+    expect(compiled).toBeInstanceOf(TagVirtualNode);
+    expect(compiled.name).toBe('div');
+  });
+});
