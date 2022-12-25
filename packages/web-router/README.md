960ea23e5e0cb7241eacee15ed4f8eb3fd9d86b6
atellmer <atellmer@gmail.com>
2022-12-24 21:38:43 -0700

    router

---
+++ packages/web-router/README.md
@@ -0,0 +1,5 @@
+# @dark-engine/web-router 🌒
+
+Dark is lightweight component-and-hook-based UI rendering engine for javascript apps without dependencies and written in Typescript 💫
+
+[README](https://github.com/atellmer/dark)
---
+++ packages/web-router/after-build.js
@@ -0,0 +1,13 @@
+const shell = require('shelljs');
+
+shell.rm('-rf', 'dist/esm/core');
+shell.rm('-rf', 'dist/cjs/core');
+shell.rm('-rf', 'dist/types/core');
+
+shell.cp('-R', 'dist/esm/web-router/src/*', 'dist/esm');
+shell.cp('-R', 'dist/cjs/web-router/src/*', 'dist/cjs');
+shell.cp('-R', 'dist/types/web-router/src/*', 'dist/types');
+
+shell.rm('-rf', 'dist/esm/web-router');
+shell.rm('-rf', 'dist/cjs/web-router');
+shell.rm('-rf', 'dist/types/web-router');
