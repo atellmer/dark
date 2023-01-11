b6f13dd0a0deea8b7cbea468dee65294f46e6260
atellmer <atellmer@gmail.com>
2023-01-11 07:33:54 -0700

    nested routes

--- examples/native/src/components/app.tsx
+++ examples/native/src/components/app.tsx
@@ -1,64 +1,140 @@
 import { h, Fragment, createComponent } from '@dark-engine/core';
 import { createStackNavigator, createBottomTabNavigator, useNavigation } from '@dark-engine/platform-native';
 
-type TestComponentProps = {
-  title: string;
-};
-
-const TestComponent = createComponent<TestComponentProps>(({ title }) => {
-  const { navigateTo, goBack, prefix } = useNavigation();
+const Profile = createComponent(() => {
+  const { navigateTo, goBack, prefix, pathname } = useNavigation();
 
   return (
-    <>
-      <stack-layout>
-        <label>{title}</label>
-        <button backgroundColor='blue' onTap={() => navigateTo(`${prefix}/Account`)}>
-          go to Account
-        </button>
-        <button backgroundColor='purple' onTap={() => navigateTo(`${prefix}/Profile`)}>
-          go to Profile
-        </button>
-        <button backgroundColor='yellow' color='black' onTap={() => navigateTo(`${prefix}/Settings`)}>
-          go to Settings
-        </button>
-        <button backgroundColor='blue' onTap={() => goBack()}>
-          back
-        </button>
-      </stack-layout>
-    </>
+    <stack-layout>
+      <label>Profile</label>
+      <button backgroundColor='purple' onTap={() => navigateTo(`${prefix}/Profile`)}>
+        Profile
+      </button>
+      <button backgroundColor='purple' onTap={() => navigateTo(`${prefix}/Account`)}>
+        Account
+      </button>
+      <button backgroundColor='purple' onTap={() => navigateTo(`${prefix}/Settings`)}>
+        Settings
+      </button>
+    </stack-layout>
   );
 });
 
-const Account = createComponent(props => <TestComponent title='Account' />);
+const Account = createComponent(() => {
+  const { navigateTo, goBack, prefix, pathname } = useNavigation();
 
-const Profile = createComponent(props => <TestComponent title='Profile' />);
+  return (
+    <stack-layout>
+      <label>Account</label>
+      <button backgroundColor='purple' onTap={() => navigateTo(`${prefix}/Profile`)}>
+        Profile
+      </button>
+      <button backgroundColor='purple' onTap={() => navigateTo(`${prefix}/Account`)}>
+        Account
+      </button>
+      <button backgroundColor='purple' onTap={() => navigateTo(`${prefix}/Settings`)}>
+        Settings
+      </button>
+    </stack-layout>
+  );
+});
 
-const Settings = createComponent(props => <TestComponent title='Settings' />);
+const Settings = createComponent(() => {
+  const { navigateTo, goBack, prefix, pathname } = useNavigation();
 
-// const Home = createComponent(props => {
-//   return (
-//     <Tab.Navigator>
-//       <Tab.Screen name='Account' component={Account} />
-//       <Tab.Screen name='Profile' component={Profile} />
-//       <Tab.Screen name='Settings' component={Settings} />
-//     </Tab.Navigator>
-//   );
-// });
+  return (
+    <stack-layout>
+      <label>Settings</label>
+      <button backgroundColor='purple' onTap={() => navigateTo(`${prefix}/Profile`)}>
+        Profile
+      </button>
+      <button backgroundColor='purple' onTap={() => navigateTo(`${prefix}/Account`)}>
+        Account
+      </button>
+      <button backgroundColor='purple' onTap={() => navigateTo(`${prefix}/Settings`)}>
+        Settings
+      </button>
+    </stack-layout>
+  );
+});
 
-const About = createComponent(() => <label>About</label>);
+const HomeStack = createStackNavigator();
 
-const Contacts = createComponent(() => <label>Contacts</label>);
+const Home = createComponent(() => {
+  const { navigateTo, goBack, prefix, pathname } = useNavigation();
+
+  return (
+    <stack-layout>
+      <label>Home</label>
+      <button backgroundColor='purple' onTap={() => navigateTo(`${prefix}/Home`)}>
+        Home
+      </button>
+      <button backgroundColor='purple' onTap={() => navigateTo(`${prefix}/About`)}>
+        About
+      </button>
+      <button backgroundColor='purple' onTap={() => navigateTo(`${prefix}/Contacts`)}>
+        Contacts
+      </button>
+      <stack-layout>
+        <HomeStack.Navigator>
+          <HomeStack.Screen name='Profile' component={Profile} options={{ headerShown: false }} />
+          <HomeStack.Screen name='Account' component={Account} options={{ headerShown: false }} />
+          <HomeStack.Screen name='Settings' component={Settings} options={{ headerShown: false }} />
+        </HomeStack.Navigator>
+      </stack-layout>
+    </stack-layout>
+  );
+});
+
+const About = createComponent(() => {
+  const { navigateTo, goBack, prefix } = useNavigation();
+
+  return (
+    <stack-layout>
+      <label>About</label>
+      <button backgroundColor='purple' onTap={() => navigateTo(`${prefix}/Home`)}>
+        Home
+      </button>
+      <button backgroundColor='purple' onTap={() => navigateTo(`${prefix}/About`)}>
+        About
+      </button>
+      <button backgroundColor='purple' onTap={() => navigateTo(`${prefix}/Contacts`)}>
+        Contacts
+      </button>
+    </stack-layout>
+  );
+});
+
+const Contacts = createComponent(() => {
+  const { navigateTo, goBack, prefix } = useNavigation();
+
+  return (
+    <stack-layout>
+      <label>Contacts</label>
+      <button backgroundColor='purple' onTap={() => navigateTo(`${prefix}/Home`)}>
+        Home
+      </button>
+      <button backgroundColor='purple' onTap={() => navigateTo(`${prefix}/About`)}>
+        About
+      </button>
+      <button backgroundColor='purple' onTap={() => navigateTo(`${prefix}/Contacts`)}>
+        Contacts
+      </button>
+    </stack-layout>
+  );
+});
 
 const Stack = createStackNavigator();
+
 const Tab = createBottomTabNavigator();
 
 const App = createComponent(() => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name='Account' component={Account} options={{ title: 'Account' }} />
-      <Tab.Screen name='Profile' component={Profile} options={{ title: 'Profile' }} />
-      <Tab.Screen name='Settings' component={Settings} options={{ title: 'Settings' }} />
-    </Tab.Navigator>
+    <Stack.Navigator>
+      <Stack.Screen name='Home' component={Home} options={{ title: 'Home' }} />
+      <Stack.Screen name='About' component={About} options={{ title: 'About' }} />
+      <Stack.Screen name='Contacts' component={Contacts} options={{ title: 'Contacts' }} />
+    </Stack.Navigator>
   );
 });
 
