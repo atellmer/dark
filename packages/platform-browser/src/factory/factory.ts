22bf3ce6c710f8f262e809292ad478f5dba00479
atellmer <atellmer@gmail.com>
2023-02-09 00:07:41 -0700

    refactoring

--- packages/platform-browser/src/factory/factory.ts
+++ packages/platform-browser/src/factory/factory.ts
@@ -2,343 +2,171 @@ import { View, type ViewDef } from '@dark-engine/core';
 
 type TagProps = Omit<ViewDef, 'as' | 'isVoid'>;
 
-const factory = (as: string) => (props?: TagProps) => View({ as, ...(props || {}) });
-
-const html = factory('html');
-const body = factory('body');
-const base = factory('base');
-const head = factory('head');
-const link = factory('link');
-const meta = factory('meta');
-const style = factory('style');
-const title = factory('title');
-const address = factory('address');
-const article = factory('article');
-const aside = factory('aside');
-const footer = factory('footer');
-const header = factory('header');
-const h1 = factory('h1');
-const h2 = factory('h2');
-const h3 = factory('h3');
-const h4 = factory('h4');
-const h5 = factory('h5');
-const h6 = factory('h6');
-const nav = factory('nav');
-const section = factory('section');
-const div = factory('div');
-const dd = factory('dd');
-const dl = factory('dl');
-const figcaption = factory('figcaption');
-const figure = factory('figure');
-const picture = factory('picture');
-const hr = factory('hr');
-const img = factory('img');
-const li = factory('li');
-const main = factory('main');
-const ol = factory('ol');
-const p = factory('p');
-const pre = factory('pre');
-const ul = factory('ul');
-const a = factory('a');
-const b = factory('b');
-const abbr = factory('abbr');
-const bdi = factory('bdi');
-const bdo = factory('bdo');
-const br = factory('br');
-const cite = factory('cite');
-const code = factory('code');
-const data = factory('data');
-const dfn = factory('dfn');
-const em = factory('em');
-const i = factory('i');
-const kbd = factory('kbd');
-const mark = factory('mark');
-const q = factory('q');
-const rp = factory('rp');
-const rt = factory('rt');
-const ruby = factory('ruby');
-const s = factory('s');
-const samp = factory('samp');
-const small = factory('small');
-const span = factory('span');
-const strong = factory('strong');
-const sub = factory('sub');
-const sup = factory('sup');
-const time = factory('time');
-const u = factory('u');
-const _var = factory('var');
-const wbr = factory('wbr');
-const area = factory('area');
-const audio = factory('audio');
-const map = factory('map');
-const track = factory('track');
-const video = factory('video');
-const embed = factory('embed');
-const object = factory('object');
-const param = factory('param');
-const source = factory('source');
-const canvas = factory('canvas');
-const script = factory('script');
-const noscript = factory('noscript');
-const del = factory('del');
-const ins = factory('ins');
-const caption = factory('caption');
-const col = factory('col');
-const colgroup = factory('colgroup');
-const table = factory('table');
-const thead = factory('thead');
-const tbody = factory('tbody');
-const td = factory('td');
-const th = factory('th');
-const tr = factory('tr');
-const button = factory('button');
-const datalist = factory('datalist');
-const fieldset = factory('fieldset');
-const form = factory('form');
-const input = factory('input');
-const label = factory('label');
-const legend = factory('legend');
-const meter = factory('meter');
-const optgroup = factory('optgroup');
-const option = factory('option');
-const output = factory('output');
-const progress = factory('progress');
-const select = factory('select');
-const textarea = factory('textarea');
-const details = factory('details');
-const dialog = factory('dialog');
-const menu = factory('menu');
-const summary = factory('summary');
-const template = factory('template');
-const blockquote = factory('blockquote');
-const iframe = factory('iframe');
-const tfoot = factory('tfoot');
-const svg = factory('svg');
-const animate = factory('animate');
-const animateMotion = factory('animateMotion');
-const animateTransform = factory('animateTransform');
-const circle = factory('circle');
-const clipPath = factory('clipPath');
-const defs = factory('defs');
-const desc = factory('desc');
-const ellipse = factory('ellipse');
-const feBlend = factory('feBlend');
-const feColorMatrix = factory('feColorMatrix');
-const feComponentTransfer = factory('feComponentTransfer');
-const feComposite = factory('feComposite');
-const feConvolveMatrix = factory('feConvolveMatrix');
-const feDiffuseLighting = factory('feDiffuseLighting');
-const feDisplacementMap = factory('feDisplacementMap');
-const feDistantLight = factory('feDistantLight');
-const feDropShadow = factory('feDropShadow');
-const feFlood = factory('feFlood');
-const feFuncA = factory('feFuncA');
-const feFuncB = factory('feFuncB');
-const feFuncG = factory('feFuncG');
-const feFuncR = factory('feFuncR');
-const feGaussianBlur = factory('feGaussianBlur');
-const feImage = factory('feImage');
-const feMerge = factory('feMerge');
-const feMergeNode = factory('feMergeNode');
-const feMorphology = factory('feMorphology');
-const feOffset = factory('feOffset');
-const fePointLight = factory('fePointLight');
-const feSpecularLighting = factory('feSpecularLighting');
-const feSpotLight = factory('feSpotLight');
-const feTile = factory('feTile');
-const feTurbulence = factory('feTurbulence');
-const filter = factory('filter');
-const foreignObject = factory('foreignObject');
-const g = factory('g');
-const image = factory('image');
-const line = factory('line');
-const linearGradient = factory('linearGradient');
-const marker = factory('marker');
-const mask = factory('mask');
-const metadata = factory('metadata');
-const mpath = factory('mpath');
-const path = factory('path');
-const pattern = factory('pattern');
-const polygon = factory('polygon');
-const polyline = factory('polyline');
-const radialGradient = factory('radialGradient');
-const rect = factory('rect');
-const stop = factory('stop');
-const _switch = factory('switch');
-const symbol = factory('symbol');
-const text = factory('text');
-const textPath = factory('textPath');
-const tspan = factory('tspan');
-const use = factory('use');
-const view = factory('view');
-
-export {
-  factory,
-  html,
-  body,
-  base,
-  head,
-  link,
-  meta,
-  style,
-  title,
-  address,
-  article,
-  aside,
-  footer,
-  header,
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6,
-  nav,
-  section,
-  div,
-  dd,
-  dl,
-  figcaption,
-  figure,
-  picture,
-  hr,
-  img,
-  li,
-  main,
-  ol,
-  p,
-  pre,
-  ul,
-  a,
-  b,
-  abbr,
-  bdi,
-  bdo,
-  br,
-  cite,
-  code,
-  data,
-  dfn,
-  em,
-  i,
-  kbd,
-  mark,
-  q,
-  rp,
-  rt,
-  ruby,
-  s,
-  samp,
-  small,
-  span,
-  strong,
-  sub,
-  sup,
-  time,
-  u,
-  _var,
-  wbr,
-  area,
-  audio,
-  map,
-  track,
-  video,
-  embed,
-  object,
-  param,
-  source,
-  canvas,
-  script,
-  noscript,
-  del,
-  ins,
-  caption,
-  col,
-  colgroup,
-  table,
-  thead,
-  tbody,
-  td,
-  th,
-  tr,
-  button,
-  datalist,
-  fieldset,
-  form,
-  input,
-  label,
-  legend,
-  meter,
-  optgroup,
-  option,
-  output,
-  progress,
-  select,
-  textarea,
-  details,
-  dialog,
-  menu,
-  summary,
-  template,
-  blockquote,
-  iframe,
-  tfoot,
-  svg,
-  animate,
-  animateMotion,
-  animateTransform,
-  circle,
-  clipPath,
-  defs,
-  desc,
-  ellipse,
-  feBlend,
-  feColorMatrix,
-  feComponentTransfer,
-  feComposite,
-  feConvolveMatrix,
-  feDiffuseLighting,
-  feDisplacementMap,
-  feDistantLight,
-  feDropShadow,
-  feFlood,
-  feFuncA,
-  feFuncB,
-  feFuncG,
-  feFuncR,
-  feGaussianBlur,
-  feImage,
-  feMerge,
-  feMergeNode,
-  feMorphology,
-  feOffset,
-  fePointLight,
-  feSpecularLighting,
-  feSpotLight,
-  feTile,
-  feTurbulence,
-  filter,
-  foreignObject,
-  g,
-  image,
-  line,
-  linearGradient,
-  marker,
-  mask,
-  metadata,
-  mpath,
-  path,
-  pattern,
-  polygon,
-  polyline,
-  radialGradient,
-  rect,
-  stop,
-  _switch,
-  symbol,
-  text,
-  textPath,
-  tspan,
-  use,
-  view,
-};
+export const factory = (as: string) => (props?: TagProps) => View({ as, ...(props || {}) });
+export const html = factory('html');
+export const body = factory('body');
+export const base = factory('base');
+export const head = factory('head');
+export const link = factory('link');
+export const meta = factory('meta');
+export const style = factory('style');
+export const title = factory('title');
+export const address = factory('address');
+export const article = factory('article');
+export const aside = factory('aside');
+export const footer = factory('footer');
+export const header = factory('header');
+export const h1 = factory('h1');
+export const h2 = factory('h2');
+export const h3 = factory('h3');
+export const h4 = factory('h4');
+export const h5 = factory('h5');
+export const h6 = factory('h6');
+export const nav = factory('nav');
+export const section = factory('section');
+export const div = factory('div');
+export const dd = factory('dd');
+export const dl = factory('dl');
+export const figcaption = factory('figcaption');
+export const figure = factory('figure');
+export const picture = factory('picture');
+export const hr = factory('hr');
+export const img = factory('img');
+export const li = factory('li');
+export const main = factory('main');
+export const ol = factory('ol');
+export const p = factory('p');
+export const pre = factory('pre');
+export const ul = factory('ul');
+export const a = factory('a');
+export const b = factory('b');
+export const abbr = factory('abbr');
+export const bdi = factory('bdi');
+export const bdo = factory('bdo');
+export const br = factory('br');
+export const cite = factory('cite');
+export const code = factory('code');
+export const data = factory('data');
+export const dfn = factory('dfn');
+export const em = factory('em');
+export const i = factory('i');
+export const kbd = factory('kbd');
+export const mark = factory('mark');
+export const q = factory('q');
+export const rp = factory('rp');
+export const rt = factory('rt');
+export const ruby = factory('ruby');
+export const s = factory('s');
+export const samp = factory('samp');
+export const small = factory('small');
+export const span = factory('span');
+export const strong = factory('strong');
+export const sub = factory('sub');
+export const sup = factory('sup');
+export const time = factory('time');
+export const u = factory('u');
+export const _var = factory('var');
+export const wbr = factory('wbr');
+export const area = factory('area');
+export const audio = factory('audio');
+export const map = factory('map');
+export const track = factory('track');
+export const video = factory('video');
+export const embed = factory('embed');
+export const object = factory('object');
+export const param = factory('param');
+export const source = factory('source');
+export const canvas = factory('canvas');
+export const script = factory('script');
+export const noscript = factory('noscript');
+export const del = factory('del');
+export const ins = factory('ins');
+export const caption = factory('caption');
+export const col = factory('col');
+export const colgroup = factory('colgroup');
+export const table = factory('table');
+export const thead = factory('thead');
+export const tbody = factory('tbody');
+export const td = factory('td');
+export const th = factory('th');
+export const tr = factory('tr');
+export const button = factory('button');
+export const datalist = factory('datalist');
+export const fieldset = factory('fieldset');
+export const form = factory('form');
+export const input = factory('input');
+export const label = factory('label');
+export const legend = factory('legend');
+export const meter = factory('meter');
+export const optgroup = factory('optgroup');
+export const option = factory('option');
+export const output = factory('output');
+export const progress = factory('progress');
+export const select = factory('select');
+export const textarea = factory('textarea');
+export const details = factory('details');
+export const dialog = factory('dialog');
+export const menu = factory('menu');
+export const summary = factory('summary');
+export const template = factory('template');
+export const blockquote = factory('blockquote');
+export const iframe = factory('iframe');
+export const tfoot = factory('tfoot');
+export const svg = factory('svg');
+export const animate = factory('animate');
+export const animateMotion = factory('animateMotion');
+export const animateTransform = factory('animateTransform');
+export const circle = factory('circle');
+export const clipPath = factory('clipPath');
+export const defs = factory('defs');
+export const desc = factory('desc');
+export const ellipse = factory('ellipse');
+export const feBlend = factory('feBlend');
+export const feColorMatrix = factory('feColorMatrix');
+export const feComponentTransfer = factory('feComponentTransfer');
+export const feComposite = factory('feComposite');
+export const feConvolveMatrix = factory('feConvolveMatrix');
+export const feDiffuseLighting = factory('feDiffuseLighting');
+export const feDisplacementMap = factory('feDisplacementMap');
+export const feDistantLight = factory('feDistantLight');
+export const feDropShadow = factory('feDropShadow');
+export const feFlood = factory('feFlood');
+export const feFuncA = factory('feFuncA');
+export const feFuncB = factory('feFuncB');
+export const feFuncG = factory('feFuncG');
+export const feFuncR = factory('feFuncR');
+export const feGaussianBlur = factory('feGaussianBlur');
+export const feImage = factory('feImage');
+export const feMerge = factory('feMerge');
+export const feMergeNode = factory('feMergeNode');
+export const feMorphology = factory('feMorphology');
+export const feOffset = factory('feOffset');
+export const fePointLight = factory('fePointLight');
+export const feSpecularLighting = factory('feSpecularLighting');
+export const feSpotLight = factory('feSpotLight');
+export const feTile = factory('feTile');
+export const feTurbulence = factory('feTurbulence');
+export const filter = factory('filter');
+export const foreignObject = factory('foreignObject');
+export const g = factory('g');
+export const image = factory('image');
+export const line = factory('line');
+export const linearGradient = factory('linearGradient');
+export const marker = factory('marker');
+export const mask = factory('mask');
+export const metadata = factory('metadata');
+export const mpath = factory('mpath');
+export const path = factory('path');
+export const pattern = factory('pattern');
+export const polygon = factory('polygon');
+export const polyline = factory('polyline');
+export const radialGradient = factory('radialGradient');
+export const rect = factory('rect');
+export const stop = factory('stop');
+export const _switch = factory('switch');
+export const symbol = factory('symbol');
+export const text = factory('text');
+export const textPath = factory('textPath');
+export const tspan = factory('tspan');
+export const use = factory('use');
+export const view = factory('view');
