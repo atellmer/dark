296b74c1272112b000f72a343eea2b39970601f4
atellmer <atellmer@gmail.com>
2023-11-11 13:18:08 +0500

    x

--- packages/platform-browser/src/factory/factory.ts
+++ packages/platform-browser/src/factory/factory.ts
@@ -2,4 +2,7 @@ import { View, type ViewDef } from '@dark-engine/core';
 
 type TagProps = Omit<ViewDef, 'as' | 'isVoid'>;
 
-export const factory = (as: string) => (props?: TagProps) => View({ as, ...(props || {}) });
+export const factory =
+  (as: string) =>
+  (props: TagProps = {}) =>
+    View({ as, ...props });
