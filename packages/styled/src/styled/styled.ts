b53e954b2f73e71817a306d6783104f3d6e44db5
atellmer <atellmer@gmail.com>
2023-12-22 20:22:06 +0500

    added aliases for styled components web

--- packages/styled/src/styled/styled.ts
+++ packages/styled/src/styled/styled.ts
@@ -42,18 +42,18 @@ function createStyledComponent<P extends StyledProps>(factory: Factory<P>) {
   let transformProps: TransformProps<P> = x => x;
   const isExtending = detectIsStyled(factory);
   const config = isExtending ? getExtendingConfig(factory as StyledComponentFactory<P>) : null;
-  const fn: Fn<P> = (source: TemplateStringsArray, ...args: Args<P>) => {
+  const fn = <T extends P>(source: TemplateStringsArray, ...args: Args<T & ThemeProps>) => {
     const $source = isExtending ? mergeTemplates(config.source, source) : source;
     const $args = isExtending ? [...config.args, ...args] : args;
-    const fns = filterArgs<P>($args);
-    const [sheet, sheets] = slice<P>(css($source, ...$args));
+    const fns = filterArgs<T>($args);
+    const [sheet, sheets] = slice<T>(css($source, ...$args));
     const [baseName, base] = generate({ sheet });
-    const styled = forwardRef<P, unknown>(
+    const styled = forwardRef<T, unknown>(
       component((props, ref) => {
         const { as: component, ...rest } = props;
         const theme = useTheme();
         const withReplace = detectIsFunction(component);
-        const $props = (withReplace ? rest : props) as unknown as P;
+        const $props = (withReplace ? rest : props) as unknown as T;
         const $factory = withReplace ? component : isExtending ? config.factory : factory;
         const [className, styles] = useMemo(() => {
           const [names, styles] = sheets.reduce(
@@ -101,13 +101,13 @@ function createStyledComponent<P extends StyledProps>(factory: Factory<P>) {
 
         return $factory({ ...transformProps($props), ref, class: className });
       }),
-    ) as StyledComponentFactory<P>;
+    ) as StyledComponentFactory<T>;
 
     styled[$$styled] = {
       className: baseName,
       source: $source,
       args: $args,
-      factory: config?.factory || factory,
+      factory: (config?.factory || factory) as Factory<T>,
     };
 
     return styled;
@@ -288,15 +288,173 @@ type StyledComponentFactory<P extends object = {}> = {
 
 type TransformProps<P> = (p: P) => any;
 
-type Fn<P extends object> = {
-  (style: TemplateStringsArray, ...args: Args<P & ThemeProps>): StyledComponentFactory<P>;
-  attrs: (t: TransformProps<P>) => Fn<P>;
-};
-
 type DynamicArgs<P> = Array<ArgFn<P>>;
 
 type ArgFn<P> = Function | ((p: P) => TextBased | false);
 
 export type Args<P> = Array<TextBased | ArgFn<P> | Keyframes>;
+
+styled.a = styled('a');
+styled.abbr = styled('abbr');
+styled.address = styled('address');
+styled.area = styled('area');
+styled.article = styled('article');
+styled.aside = styled('aside');
+styled.audio = styled('audio');
+styled.b = styled('b');
+styled.base = styled('base');
+styled.bdi = styled('bdi');
+styled.bdo = styled('bdo');
+styled.blockquote = styled('blockquote');
+styled.body = styled('body');
+styled.br = styled('br');
+styled.button = styled('button');
+styled.canvas = styled('canvas');
+styled.caption = styled('caption');
+styled.cite = styled('cite');
+styled.code = styled('code');
+styled.col = styled('col');
+styled.colgroup = styled('colgroup');
+styled.data = styled('data');
+styled.datalist = styled('datalist');
+styled.dd = styled('dd');
+styled.del = styled('del');
+styled.details = styled('details');
+styled.dfn = styled('dfn');
+styled.dialog = styled('dialog');
+styled.div = styled('div');
+styled.dl = styled('dl');
+styled.dt = styled('dt');
+styled.em = styled('em');
+styled.embed = styled('embed');
+styled.fieldset = styled('fieldset');
+styled.figcaption = styled('figcaption');
+styled.figure = styled('figure');
+styled.footer = styled('footer');
+styled.form = styled('form');
+styled.h1 = styled('h1');
+styled.h2 = styled('h2');
+styled.h3 = styled('h3');
+styled.h4 = styled('h4');
+styled.h5 = styled('h5');
+styled.h6 = styled('h6');
+styled.head = styled('head');
+styled.header = styled('header');
+styled.hgroup = styled('hgroup');
+styled.hr = styled('hr');
+styled.html = styled('html');
+styled.i = styled('i');
+styled.iframe = styled('iframe');
+styled.img = styled('img');
+styled.input = styled('input');
+styled.ins = styled('ins');
+styled.kbd = styled('kbd');
+styled.label = styled('label');
+styled.legend = styled('legend');
+styled.li = styled('li');
+styled.link = styled('link');
+styled.main = styled('main');
+styled.map = styled('map');
+styled.mark = styled('mark');
+styled.menu = styled('menu');
+styled.meta = styled('meta');
+styled.meter = styled('meter');
+styled.nav = styled('nav');
+styled.noscript = styled('noscript');
+styled.object = styled('object');
+styled.ol = styled('ol');
+styled.optgroup = styled('optgroup');
+styled.option = styled('option');
+styled.output = styled('output');
+styled.p = styled('p');
+styled.param = styled('param');
+styled.picture = styled('picture');
+styled.pre = styled('pre');
+styled.progress = styled('progress');
+styled.q = styled('q');
+styled.rp = styled('rp');
+styled.rt = styled('rt');
+styled.ruby = styled('ruby');
+styled.s = styled('s');
+styled.samp = styled('samp');
+styled.script = styled('script');
+styled.section = styled('section');
+styled.select = styled('select');
+styled.small = styled('small');
+styled.source = styled('source');
+styled.span = styled('span');
+styled.strong = styled('strong');
+styled.style = styled('style');
+styled.sub = styled('sub');
+styled.summary = styled('summary');
+styled.sup = styled('sup');
+styled.table = styled('table');
+styled.tbody = styled('tbody');
+styled.td = styled('td');
+styled.template = styled('template');
+styled.textarea = styled('textarea');
+styled.tfoot = styled('tfoot');
+styled.th = styled('th');
+styled.thead = styled('thead');
+styled.time = styled('time');
+styled.title = styled('title');
+styled.tr = styled('tr');
+styled.track = styled('track');
+styled.u = styled('u');
+styled.ul = styled('ul');
+styled.var = styled('var');
+styled.video = styled('video');
+styled.wbr = styled('wbr');
+styled.svg = styled('svg');
+styled.circle = styled('circle');
+styled.clipPath = styled('clipPath');
+styled.defs = styled('defs');
+styled.desc = styled('desc');
+styled.ellipse = styled('ellipse');
+styled.feBlend = styled('feBlend');
+styled.feColorMatrix = styled('feColorMatrix');
+styled.feComponentTransfer = styled('feComponentTransfer');
+styled.feComposite = styled('feComposite');
+styled.feConvolveMatrix = styled('feConvolveMatrix');
+styled.feDiffuseLighting = styled('feDiffuseLighting');
+styled.feDisplacementMap = styled('feDisplacementMap');
+styled.feDistantLight = styled('feDistantLight');
+styled.feFlood = styled('feFlood');
+styled.feFuncA = styled('feFuncA');
+styled.feFuncB = styled('feFuncB');
+styled.feFuncG = styled('feFuncG');
+styled.feFuncR = styled('feFuncR');
+styled.feGaussianBlur = styled('feGaussianBlur');
+styled.feImage = styled('feImage');
+styled.feMerge = styled('feMerge');
+styled.feMergeNode = styled('feMergeNode');
+styled.feMorphology = styled('feMorphology');
+styled.feOffset = styled('feOffset');
+styled.fePointLight = styled('fePointLight');
+styled.feSpecularLighting = styled('feSpecularLighting');
+styled.feSpotLight = styled('feSpotLight');
+styled.feTile = styled('feTile');
+styled.feTurbulence = styled('feTurbulence');
+styled.filter = styled('filter');
+styled.g = styled('g');
+styled.image = styled('image');
+styled.line = styled('line');
+styled.linearGradient = styled('linearGradient');
+styled.marker = styled('marker');
+styled.mask = styled('mask');
+styled.path = styled('path');
+styled.pattern = styled('pattern');
+styled.polygon = styled('polygon');
+styled.polyline = styled('polyline');
+styled.radialGradient = styled('radialGradient');
+styled.rect = styled('rect');
+styled.stop = styled('stop');
+styled.switch = styled('switch');
+styled.symbol = styled('symbol');
+styled.text = styled('text');
+styled.textPath = styled('textPath');
+styled.tspan = styled('tspan');
+styled.use = styled('use');
+styled.view = styled('view');
 
 export { setupGlobal, styled, css, inject, filterArgs, detectIsStyled };
