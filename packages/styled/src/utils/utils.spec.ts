a8ec9fd21ee30c3250e746733d2aba7c08e56818
atellmer <atellmer@gmail.com>
2023-12-22 13:01:18 +0500

    added utils tests

--- packages/styled/src/utils/utils.spec.ts
+++ packages/styled/src/utils/utils.spec.ts
@@ -1,4 +1,13 @@
-import { uniq } from './utils';
+import {
+  uniq,
+  mapProps,
+  mergeClassNames,
+  getElement,
+  createStyleElement,
+  setAttr,
+  append,
+  mergeTemplates,
+} from './utils';
 
 describe('[@styled/utils]', () => {
   test('the uniq function works correctly', () => {
@@ -11,5 +20,65 @@ describe('[@styled/utils]', () => {
       { id: 2 },
       { id: 3 },
     ]);
+  });
+
+  test('the mapProps function works correctly', () => {
+    expect(typeof mapProps).toBe('function');
+    expect(mapProps({ a: 1, b: 2, c: 3 })).toEqual([1, 2, 3]);
+    expect(mapProps({ a: {}, b: [], c: '3' })).toEqual([{}, [], '3']);
+  });
+
+  test('the mergeClassNames function works correctly', () => {
+    expect(typeof mergeClassNames).toBe('function');
+    expect(mergeClassNames(['a', 'b', 'c', 'c', 'a', 'item'])).toBe('a b c item');
+  });
+
+  test('the getElement function works correctly', () => {
+    expect(typeof getElement).toBe('function');
+    expect(getElement('html body')).toBe(document.body);
+  });
+
+  test('the createStyleElement function works correctly', () => {
+    expect(typeof createStyleElement).toBe('function');
+    expect(createStyleElement()).toBeInstanceOf(document.createElement('style').constructor);
+  });
+
+  test('the setAttr function works correctly', () => {
+    expect(typeof setAttr).toBe('function');
+
+    const element = document.createElement('div');
+    const attrName = 'some-attr';
+    const attrValue = '123';
+
+    setAttr(element, attrName, attrValue);
+    expect(element.getAttribute(attrName)).toBe(attrValue);
+  });
+
+  test('the append function works correctly', () => {
+    expect(typeof append).toBe('function');
+
+    const parent = document.createElement('div');
+    const child = document.createElement('span');
+
+    append(parent, child);
+    expect(parent.innerHTML).toBe('<span></span>');
+  });
+
+  test('the mergeTemplates function works correctly', () => {
+    expect(typeof mergeTemplates).toBe('function');
+    const template = (strings: TemplateStringsArray, ...args: Array<any>) => strings;
+    const t1 = template`
+      color: red;
+      background-color: ${'blue'};
+    `;
+    const t2 = template`
+      font-size: ${24}px;
+    `;
+    const t3 = mergeTemplates(t1, t2);
+
+    expect(JSON.stringify(t3)).toEqual(
+      JSON.stringify(['\n      color: red;\n      background-color: ', ';\n    \n      font-size: ', 'px;\n    ']),
+    );
+    expect(t3.raw).toBeDefined();
   });
 });
