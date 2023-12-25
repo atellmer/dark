81356451634ca062400d54e4bdf117ddbc3dfd63
atellmer <atellmer@gmail.com>
2023-12-25 16:18:39 +0500

    fix

--- examples/server-side-rendering/client/static/704.build.js
+++ examples/server-side-rendering/client/static/704.build.js
@@ -1,2 +1,24 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[704],{38:(e,t,r)=>{r.d(t,{G:()=>a});var u=r(654),o=r(524);const a=(0,u.wU)((({slot:e})=>(0,o.h)("article",{class:"fade animated-route"},e)))},704:(e,t,r)=>{r.r(t),r.d(t,{default:()=>b});var u=r(654),o=r(750),a=r(238),i=r(524),n=r(218),s=r(113),l=r(397),c=r(940),h=r(450),m=r(752),d=r(169),p=r(38);const b=(0,u.wU)((({slot:e})=>{const{url:t}=function(){const e=(0,s.M6)();(0,s.BU)(e);const t=(0,s._L)(),{location:{pathname:r}}=e,u=(0,n.Y)((()=>t?(0,l.L7)(r,t):""),[t,r]);return{path:t,url:u}}(),r=(0,c.k)(),u=(0,h.U)(),b=(0,m.T)(),[f,k]=(0,o.e)(0);return(0,a.d4)((()=>{"null"===u.get("id")&&r.push("/home/888")}),[b.url]),(0,i.h)(p.G,null,(0,i.h)("h1",null,"Home"),(0,i.h)("header",null,(0,i.h)(d.r,{to:`${t}a`},"child route a"),(0,i.h)(d.r,{to:`${t}b`},"child route b"),(0,i.h)(d.r,{to:`${t}c`},"child route c"),(0,i.h)("button",{onClick:()=>k(f+1)},"fired ",f," times")),(0,i.h)("p",null,"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero excepturi quae harum laborum temporibus? Repellendus laboriosam sunt corporis quasi. Quo accusamus aperiam consequuntur quia veritatis nobis minima omnis error expedita!"),e)}))},450:(e,t,r)=>{r.d(t,{U:()=>o});var u=r(113);function o(){const e=(0,u.M6)();return(0,u.BU)(e),e.params}}}]);
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[704],{38:(r,o,e)=>{e.d(o,{G:()=>l});var a=e(654),t=e(524);const l=(0,a.wU)((({slot:r})=>(0,t.h)("article",{class:"fade animated-route"},r)))},704:(r,o,e)=>{e.r(o),e.d(o,{default:()=>f});var a=e(654),t=e(750),l=e(238),i=e(524),u=e(218),n=e(113),c=e(397),s=e(940),d=e(450),h=e(752),m=e(169),p=e(550),b=e(38);const v=p.zo.button`
+  display: inline-block;
+  font-size: 1rem;
+  padding: 0.5rem 0.7rem;
+  background-color: var(--color);
+  color: var(--text-color);
+  border: 1px solid var(--color);
+  border-radius: 3px;
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    background-color: var(--hover-color);
+  }
+  &:active {
+    background-color: var(--color);
+  }
+
+  ${r=>p.iv`
+    --color: ${r.$primary?"#BA68C8":"#eee"};
+    --hover-color: ${r.$primary?"#8E24AA":"#E0E0E0"};
+    --text-color: ${r.$primary?"#fff":"#000"};
+  `}
+`,f=(0,a.wU)((({slot:r})=>{const{url:o}=function(){const r=(0,n.M6)();(0,n.BU)(r);const o=(0,n._L)(),{location:{pathname:e}}=r,a=(0,u.Y)((()=>o?(0,c.L7)(e,o):""),[o,e]);return{path:o,url:a}}(),e=(0,s.k)(),a=(0,d.U)(),p=(0,h.T)(),[f,$]=(0,t.e)(0);return(0,l.d4)((()=>{"null"===a.get("id")&&e.push("/home/888")}),[p.url]),(0,i.h)(b.G,null,(0,i.h)("h1",null,"Home"),(0,i.h)("header",null,(0,i.h)(m.r,{to:`${o}a`},"child route a"),(0,i.h)(m.r,{to:`${o}b`},"child route b"),(0,i.h)(m.r,{to:`${o}c`},"child route c"),(0,i.h)("button",{onClick:()=>$(f+1)},"fired ",f," times")),(0,i.h)("p",null,"Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero excepturi quae harum laborum temporibus? Repellendus laboriosam sunt corporis quasi. Quo accusamus aperiam consequuntur quia veritatis nobis minima omnis error expedita!"),(0,i.h)(v,null,"Default"),(0,i.h)(v,{$primary:!0},"Primary"),r)}))},450:(r,o,e)=>{e.d(o,{U:()=>t});var a=e(113);function t(){const r=(0,a.M6)();return(0,a.BU)(r),r.params}}}]);
 //# sourceMappingURL=704.build.js.map
