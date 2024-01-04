19e27dff750f45c3aaa8822c4c4ed304271dfd3a
atellmer <atellmer@gmail.com>
2024-01-04 15:45:26 +0500

    added transition tests

--- packages/core/src/start-transition/start-transition.spec.tsx
+++ packages/core/src/start-transition/start-transition.spec.tsx
@@ -20,8 +20,8 @@ describe('@core/start-transition', () => {
   test('can make a transition via start-transition', async () => {
     const size = 10;
     const content = (idx: number) => dom`
-      ${idx === 0 ? `<normal-tab>1</normal-tab>${replacer}${replacer}` : ''}
-      ${idx === 1 ? `${replacer}<normal-tab>2</normal-tab>${replacer}` : ''}
+      ${idx === 0 ? `<normal-tab-1></normal-tab-1>${replacer}${replacer}` : ''}
+      ${idx === 1 ? `${replacer}<normal-tab-2></normal-tab-2>${replacer}` : ''}
       ${
         idx === 2
           ? `${replacer}${replacer}<slow-tab>${Array(size)
@@ -32,8 +32,8 @@ describe('@core/start-transition', () => {
       }
     `;
     let setIdx: (x: number) => void = null;
-    const NormalTab1 = component(() => <normal-tab>1</normal-tab>);
-    const NormalTab2 = component(() => <normal-tab>2</normal-tab>);
+    const NormalTab1 = component(() => <normal-tab-1></normal-tab-1>);
+    const NormalTab2 = component(() => <normal-tab-2></normal-tab-2>);
     const SlowTab = memo(
       component(() => {
         const items = [];
@@ -45,7 +45,6 @@ describe('@core/start-transition', () => {
         return <slow-tab>{items}</slow-tab>;
       }),
     );
-
     const SlowItem = component(() => {
       const startTime = getTime();
 
@@ -55,7 +54,6 @@ describe('@core/start-transition', () => {
 
       return null;
     });
-
     const App = component(() => {
       const [idx, _setIdx] = useState(0);
 
@@ -130,8 +128,8 @@ describe('@core/start-transition', () => {
     const size = 10;
     const content = (idx: number, isPending = false) => dom`
       <div class="${isPending ? 'pending' : 'normal'}">
-        ${idx === 0 ? `<normal-tab>1</normal-tab>${replacer}${replacer}` : ''}
-        ${idx === 1 ? `${replacer}<normal-tab>2</normal-tab>${replacer}` : ''}
+        ${idx === 0 ? `<normal-tab-1></normal-tab-1>${replacer}${replacer}` : ''}
+        ${idx === 1 ? `${replacer}<normal-tab-2></normal-tab-2>${replacer}` : ''}
         ${
           idx === 2
             ? `${replacer}${replacer}<slow-tab>${Array(size)
@@ -144,8 +142,8 @@ describe('@core/start-transition', () => {
     `;
     let setIdx: (x: number) => void = null;
     let startTransition: (cb: Function) => void = null;
-    const NormalTab1 = component(() => <normal-tab>1</normal-tab>);
-    const NormalTab2 = component(() => <normal-tab>2</normal-tab>);
+    const NormalTab1 = component(() => <normal-tab-1></normal-tab-1>);
+    const NormalTab2 = component(() => <normal-tab-2></normal-tab-2>);
     const SlowTab = memo(
       component(() => {
         const items = [];
@@ -157,7 +155,6 @@ describe('@core/start-transition', () => {
         return <slow-tab>{items}</slow-tab>;
       }),
     );
-
     const SlowItem = component(() => {
       const startTime = getTime();
 
@@ -167,7 +164,6 @@ describe('@core/start-transition', () => {
 
       return null;
     });
-
     const App = component(() => {
       const [isPending, _startTransition] = useTransition();
       const [idx, _setIdx] = useState(0);
@@ -238,5 +234,68 @@ describe('@core/start-transition', () => {
 
     await sleep(100);
     expect(host.innerHTML).toBe(content(2));
+  });
+
+  test('can cancel the same updates', async () => {
+    const size = 20;
+    const content = (marker: string) => dom`
+      <div>${marker}:${Array(size)
+      .fill(null)
+      .map(() => replacer)
+      .join('')}
+      </div>
+    `;
+    let setMarker: (x: string) => void = null;
+    const Slow = memo(
+      component<{ marker: string }>(({ marker }) => {
+        const items = [];
+
+        for (let i = 0; i < size; i++) {
+          items.push(<SlowItem key={i} />);
+        }
+
+        return (
+          <div>
+            {marker}:{items}
+          </div>
+        );
+      }),
+    );
+    const SlowItem = component(() => {
+      const startTime = getTime();
+
+      while (getTime() - startTime < 1) {
+        //
+      }
+
+      return null;
+    });
+    const App = component(() => {
+      const [marker, _setMarker] = useState('a');
+
+      setMarker = _setMarker;
+
+      return <Slow marker={marker} />;
+    });
+
+    render(<App />);
+    expect(host.innerHTML).toBe(content('a'));
+
+    startTransition(() => setMarker('b'));
+    expect(host.innerHTML).toBe(content('a'));
+
+    await sleep(1);
+    expect(host.innerHTML).toBe(content('a'));
+
+    startTransition(() => setMarker('c'));
+    await sleep(1);
+    expect(host.innerHTML).toBe(content('a'));
+
+    startTransition(() => setMarker('d'));
+    await sleep(1);
+    expect(host.innerHTML).toBe(content('a'));
+
+    await sleep(200);
+    expect(host.innerHTML).toBe(content('d'));
   });
 });
