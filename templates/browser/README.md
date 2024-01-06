830db99002f45a8e5717fbe3809f295eb379ae2b
atellmer <atellmer@gmail.com>
2024-01-06 15:06:04 +0500

    fix

---
+++ templates/browser/README.md
@@ -0,0 +1,11 @@
+## Dark browser template
+
+```
+npx degit github:atellmer/dark/templates/browser browser-app
+```
+
+```
+cd browser-app
+npm i
+npm start
+```
--- templates/browser/src/index.tsx
+++ templates/browser/src/index.tsx
@@ -1,15 +1,35 @@
 import { h, Fragment, component, useState } from '@dark-engine/core';
 import { createRoot } from '@dark-engine/platform-browser';
+import { createGlobalStyle } from '@dark-engine/styled';
 
 const App = component(() => {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <GlobalStyle />
       <h1>Hello Dark: {count}</h1>
       <button onClick={() => setCount(x => x + 1)}>increment!</button>
     </>
   );
 });
+
+const GlobalStyle = createGlobalStyle`
+  *, *::after, *::before {
+    box-sizing: border-box;
+  }
+
+  html {
+    font-size: 14px;
+  }
+
+  html, body {
+    padding: 0;
+    margin: 0;
+    font-family: sans-serif;
+    width: 100%;
+    height: 100%;
+  }
+`;
 
 createRoot(document.getElementById('root')).render(<App />);
