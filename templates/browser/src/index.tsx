8937ac10fd64c64727e9e8af4021533045ebac06
atellmer <atellmer@gmail.com>
2024-01-21 16:38:25 +0500

    refactoring

--- templates/browser/src/index.tsx
+++ templates/browser/src/index.tsx
@@ -1,18 +1,14 @@
-import { h, Fragment, component, useAtom, useComputed, useStore } from '@dark-engine/core';
+import { h, Fragment, component, useAtom } from '@dark-engine/core';
 import { createRoot } from '@dark-engine/platform-browser';
 import { createGlobalStyle } from '@dark-engine/styled';
 
 const App = component(() => {
   const a$ = useAtom(0);
-  const b$ = useComputed([a$], a => a ** 2);
-  const [a, b] = useStore([a$, b$]);
 
   return (
     <>
       <GlobalStyle />
-      <h1>
-        {a} ^ 2 = {b}
-      </h1>
+      <h1>{a$.val()}</h1>
       <button onClick={() => a$.set(x => x + 1)}>increment</button>
     </>
   );
@@ -24,15 +20,18 @@ const GlobalStyle = createGlobalStyle`
   }
 
   html {
-    font-size: 14px;
+    font-size: 16px;
   }
 
   html, body {
     padding: 0;
     margin: 0;
-    font-family: sans-serif;
     width: 100%;
-    height: 100%;
+  }
+
+  body {
+    font-family: sans-serif;
+    padding: 16px;
   }
 `;
 
