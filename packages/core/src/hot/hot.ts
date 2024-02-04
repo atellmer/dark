0b3b80e4e48c267326995113ea1e05d97ba14936
atellmer <atellmer@gmail.com>
2024-02-04 20:18:03 +0500

    refactoring

--- packages/core/src/hot/hot.ts
+++ packages/core/src/hot/hot.ts
@@ -1,10 +1,11 @@
 import { $$scope } from '../scope';
 
-function $hot(update: () => void) {
+function hot(update: () => void) {
   if (process.env.NODE_ENV !== 'production') {
     process.env.NODE_ENV === 'development' && $$scope().setIsHot(true);
   }
+
   update();
 }
 
-export { $hot as hot };
+export { hot };
