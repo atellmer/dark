151561262bd3b0e7beb714d53b73f5acecfec1a4
atellmer <atellmer@gmail.com>
2024-05-20 12:54:24 +0500

    example

--- examples/spring-dialog/dialog.tsx
+++ examples/spring-dialog/dialog.tsx
@@ -1,20 +1,22 @@
-import { type DarkElement, component, useMemo, useLayoutEffect } from '@dark-engine/core';
-import { createPortal } from '@dark-engine/platform-browser';
+import { type DarkElement, component, useMemo, useLayoutEffect, useRef, __useSSR as useSSR } from '@dark-engine/core';
+import { type DarkJSX, createPortal } from '@dark-engine/platform-browser';
 import { styled } from '@dark-engine/styled';
 import { type SpringValue, Animated, useTransition } from '@dark-engine/animations';
 
-type SpringProps = 'opacity' | 'scale';
-
 type DialogProps = {
   isOpen: boolean;
+  width?: string;
+  height?: string;
   slot: DarkElement;
   onRequestClose: () => void;
 };
 
-const Dialog = component<DialogProps>(({ isOpen, slot, onRequestClose }) => {
-  const host = useMemo(() => document.createElement('div'), []);
+const Dialog = component<DialogProps>(({ isOpen, width = '600px', height = '300px', slot, onRequestClose }) => {
+  const { isServer, isSSR } = useSSR();
+  const host = useMemo(() => (isServer ? null : document.createElement('div')), []);
+  const rootRef = useRef<HTMLDivElement>(null);
   const items = useMemo(() => [isOpen].filter(Boolean), [isOpen]);
-  const scope = useMemo(() => ({ isOpen }), []);
+  const scope = useMemo<Scope>(() => ({ isOpen, container: null }), []);
   const [transition, api] = useTransition<SpringProps, boolean>(
     items,
     x => String(x),
@@ -22,11 +24,12 @@ const Dialog = component<DialogProps>(({ isOpen, slot, onRequestClose }) => {
       from: { opacity: 0, scale: 0 },
       enter: { opacity: 1, scale: 1 },
       leave: { opacity: 0, scale: 0 },
-      config: () => ({ tension: 160, friction: 24 }),
+      config: () => ({ tension: 160, friction: 24, precision: 4 }),
     }),
   );
 
   scope.isOpen = isOpen;
+  scope.container = rootRef.current;
 
   useLayoutEffect(() => {
     const offs = [
@@ -40,16 +43,21 @@ const Dialog = component<DialogProps>(({ isOpen, slot, onRequestClose }) => {
     };
   }, []);
 
+  if (isSSR) return null;
+
   return createPortal(
-    <Container>
+    <Container ref={rootRef}>
       {transition(({ spring }) => (
-        <Animated spring={spring} fn={styleFn(true)}>
+        <Animated spring={spring} fn={styleFn(true, scope)}>
           <Overlay onClick={onRequestClose} />
         </Animated>
       ))}
       {transition(({ spring }) => (
-        <Animated spring={spring} fn={styleFn(false)}>
-          <Content>{slot}</Content>
+        <Animated spring={spring} fn={styleFn(false, scope)}>
+          <Window $width={width} $height={height}>
+            <CloseButton onClick={onRequestClose} />
+            {slot}
+          </Window>
         </Animated>
       ))}
     </Container>,
@@ -57,25 +65,52 @@ const Dialog = component<DialogProps>(({ isOpen, slot, onRequestClose }) => {
   );
 });
 
-const styleFn = (isOverlay: boolean) => (element: HTMLDivElement, value: SpringValue<SpringProps>) => {
+const styleFn = (isOverlay: boolean, scope: Scope) => (element: HTMLDivElement, value: SpringValue<SpringProps>) => {
   const { opacity, scale } = value;
-  const setProp = setPropOf(element);
+  const { isOpen, container } = scope;
+  const setElementProp = setPropOf(element);
+  const removeElementProp = removePropOf(element);
 
   if (isOverlay) {
-    setProp('opacity', `${opacity}`);
+    const setContainerProp = container ? setPropOf(container) : null;
+    const removeContainerProp = container ? removePropOf(container) : null;
 
-    if (opacity < 0.5) {
-      setProp('pointer-events', `none`);
+    if (isOpen) {
+      opacity < 0.95 && setElementProp('opacity', `${opacity}`);
     } else {
-      element.style.removeProperty('pointer-events');
+      opacity < 0.95 && setElementProp('opacity', `${opacity}`);
+    }
+
+    if (!isOpen) {
+      setElementProp('pointer-events', 'none');
+      setContainerProp && setContainerProp('pointer-events', 'none');
+    } else {
+      removeElementProp('pointer-events');
+      removeContainerProp && removeContainerProp('pointer-events');
     }
   } else {
-    setProp('opacity', `${opacity}`);
-    setProp('transform', `scale(${scale})`);
+    setElementProp('opacity', `${opacity}`);
+    setElementProp('transform', `scale(${scale})`);
   }
 };
 
 const setPropOf = (element: HTMLElement) => element.style.setProperty.bind(element.style);
+
+const removePropOf = (element: HTMLElement) => element.style.removeProperty.bind(element.style);
+
+type CloseButtonProps = {
+  onClick: () => void;
+};
+
+const CloseButton = component<CloseButtonProps>(({ onClick }) => {
+  return (
+    <Button onClick={onClick}>
+      <svg width='24' height='24' viewBox='0 0 50 50'>
+        <path d='M 9.15625 6.3125 L 6.3125 9.15625 L 22.15625 25 L 6.21875 40.96875 L 9.03125 43.78125 L 25 27.84375 L 40.9375 43.78125 L 43.78125 40.9375 L 27.84375 25 L 43.6875 9.15625 L 40.84375 6.3125 L 25 22.15625 Z' />
+      </svg>
+    </Button>
+  );
+});
 
 const Container = styled.div`
   position: fixed;
@@ -87,26 +122,50 @@ const Container = styled.div`
   padding: 16px;
 `;
 
-const Content = styled.div`
+const Button = styled.button`
+  cursor: pointer;
+  background-color: transparent;
+  border: none;
+  padding: 4px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  stroke-width: 4px;
+  stroke: #303f9f;
+`;
+
+const Window = styled.div<{ $width: string; $height: string } & DarkJSX.Elements['div']>`
   position: relative;
-  width: 600px;
-  height: 300px;
+  width: ${p => p.$width};
+  min-height: ${p => p.$height};
   max-width: 100%;
   background-color: #fff;
   border-radius: 4px;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
-  padding: 16px;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  padding: 32px;
   will-change: opacity, transform;
   margin: auto;
+  transform-origin: 50% 50%;
+
+  & ${Button} {
+    position: absolute;
+    top: 8px;
+    right: 8px;
+  }
 `;
 
 const Overlay = styled.div`
   position: fixed;
   width: 100%;
-  height: 100%;
+  height: 200%;
   inset: 0;
   background-color: #303f9f;
   cursor: pointer;
+  will-change: opacity;
 `;
+
+type SpringProps = 'opacity' | 'scale';
+
+type Scope = { isOpen: boolean; container: HTMLDivElement };
 
 export { Dialog };
--- examples/spring-dialog/index.tsx
+++ examples/spring-dialog/index.tsx
@@ -10,11 +10,24 @@ const App = component(() => {
   return (
     <>
       <GlobalStyle />
-      <button onClick={() => setIsOpen(x => !x)}>toggle</button>
+      <Button onClick={() => setIsOpen(x => !x)}>Toggle dialog</Button>
       <Dialog isOpen={isOpen} onRequestClose={() => setIsOpen(false)}>
-        <Container>
+        <Content>
           <h1>Hey, I'm a dialog! 👋</h1>
-        </Container>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo soluta vero unde, cumque perspiciatis mollitia
+            sunt exercitationem beatae assumenda iusto eos fugit fugiat alias est explicabo pariatur. Reprehenderit
+            eligendi quae molestias nesciunt dolore sequi qui delectus nobis unde, officia, numquam dignissimos sit
+            voluptas necessitatibus consectetur maxime maiores tempora aliquid nihil ducimus reiciendis architecto
+          </p>
+          <img src='./assets/mountains.jpg' alt='mountains' />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo soluta vero unde, cumque perspiciatis mollitia
+            sunt exercitationem beatae assumenda iusto eos fugit fugiat alias est explicabo pariatur. Reprehenderit
+            eligendi quae molestias nesciunt dolore sequi qui delectus nobis unde, officia, numquam dignissimos sit
+            voluptas necessitatibus consectetur maxime maiores tempora aliquid nihil ducimus reiciendis architecto
+          </p>
+        </Content>
       </Dialog>
     </>
   );
@@ -38,13 +51,41 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-const Container = styled.main`
-  display: flex;
+const Button = styled.button`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 16px;
+  border: none;
+  text-transform: uppercase;
+  background-color: #e91e63;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  cursor: pointer;
+  color: #fff;
+  border-radius: 4px;
+  letter-spacing: 1px;
+  font-weight: 700;
+  transition: opacity 0.05s ease-in-out;
+
+  &:active {
+    opacity: 0.5;
+  }
+`;
+
+const Content = styled.main`
   width: 100%;
   height: 100%;
-  justify-content: center;
-  align-items: center;
   text-align: center;
+
+  & p {
+    text-align: justify;
+    margin: 32px 0;
+  }
+
+  & img {
+    width: 100%;
+  }
 `;
 
 createRoot(document.getElementById('root')).render(<App />);
