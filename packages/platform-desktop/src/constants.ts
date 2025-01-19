deb2e74709abde45e188502429fa3198d8a15c7f
Alex Plex <atellmer@gmail.com>
2025-01-19 13:07:38 +0500

    feat: release version 1.5.0

--- packages/platform-desktop/src/constants.ts
+++ packages/platform-desktop/src/constants.ts
@@ -1,4 +1,4 @@
-export const VERSION = '1.4.2';
+export const VERSION = '1.5.0';
 export const LIB = '@dark-engine/platform-desktop';
 export const TEXT_ATTR = 'text';
 export const HIDDEN_ATTR = 'hidden';
